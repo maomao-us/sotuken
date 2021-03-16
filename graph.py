dce2265edff4bd5db4003b2dd7dbca1bbd86e30c
maomao-us <79571542+maomao-us@users.noreply.github.com>
2021-03-16 11:32:45 +0900

    Add files via upload

--- graph.py
+++ graph.py
@@ -7,13 +7,13 @@ from sklearn.metrics import r2_score
 
 #CSVファイルを読み込む
 csv_x = pd.read_csv('Nx.csv')
-print(len(csv_x))
+
 # len(csv_x) = 600
 
 plot_x = []
-# plot_y1 = []
-# plot_y2 = []
-# plot_y3 = []
+plot_x1 = []
+plot_x2 = []
+plot_x3 = []
 
 # time
 for i in range(len(csv_x)):
@@ -21,40 +21,50 @@ for i in range(len(csv_x)):
 
 # acc x1
 # for i in range(len(csv_x)):
-#     plot_y1 += [csv_x.iloc[i,1]]
+#     plot_x1 += [csv_x.iloc[i,1]]
 
 # acc x2
 # for i in range(len(csv_x)):
-#     plot_y2 += [csv_x.iloc[i,2]]
+#     plot_x2 += [csv_x.iloc[i,2]]
 
 # acc x3
 # for i in range(len(csv_x)):
-#     plot_y3 += [csv_x.iloc[i,3]]
+#     plot_x3 += [csv_x.iloc[i,3]]
 
 
 
 window = 10
-plot_z1 = csv_x['Linear Acceleration x1 (m/s^2)'].rolling(window).mean()
-plot_z2 = csv_x['Linear Acceleration x2 (m/s^2)'].rolling(window).mean()
-plot_z3 = csv_x['Linear Acceleration x3 (m/s^2)'].rolling(window).mean()
+plot_xm1 = csv_x['Linear Acceleration x1 (m/s^2)'].rolling(window).mean()
+plot_xm2 = csv_x['Linear Acceleration x2 (m/s^2)'].rolling(window).mean()
+plot_xm3 = csv_x['Linear Acceleration x3 (m/s^2)'].rolling(window).mean()
 
 for i in range(0,window - 1):
-    plot_z1[i] = 0
-    plot_z2[i] = 0
-    plot_z3[i] = 0
+    plot_xm1[i] = 0
+    plot_xm2[i] = 0
+    plot_xm3[i] = 0
 
-plot_roll = np.roll(plot_z1,-10)
+count = 0
+plot_roll = 0
+plot_maxroll = np.roll(plot_xm1, 0)
 
-print(r2_score(plot_roll, plot_z2))
+for i in range(1,len(csv_x)):
+    plot_roll = np.roll(plot_xm1,-i)
+    if r2_score(plot_roll, plot_xm2) > r2_score(plot_maxroll, plot_xm2):
+        plot_maxroll = plot_roll
+        count = i
 
-# plt.plot(plot_x,plot_y1)
-# plt.plot(plot_x,plot_y2)
-# plt.plot(plot_x,plot_y3)
+print("\n")
+print(count)
+print(r2_score(plot_maxroll, plot_xm2))
 
-# plt.plot(plot_x,plot_z1)
-plt.plot(plot_x, plot_roll)
-plt.plot(plot_x,plot_z2)
-# plt.plot(plot_x,plot_z3)
+# plt.plot(plot_x,plot_x1)
+# plt.plot(plot_x,plot_x2)
+# plt.plot(plot_x,plot_x3)
+
+# plt.plot(plot_x,plot_xm1)
+plt.plot(plot_x, plot_maxroll)
+plt.plot(plot_x,plot_xm2)
+# plt.plot(plot_x,plot_xm3)
 
 plt.show()
 
